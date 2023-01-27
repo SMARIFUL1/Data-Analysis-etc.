0fc9eeda4475e7e587993cdb75a5236dcb40388c
Sayed Arif <115746081+SMARIFUL1@users.noreply.github.com>
2023-01-28 00:22:13 +0100

    Add files via upload
    Queries

---
+++ SQL + Power BI/Dim_Calendar.sql
@@ -0,0 +1,25 @@
+--Cleansed Dim_Date table--
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  --,[DayNumberOfWeek]
+  [EnglishDayNameOfWeek] AS Day, 
+  --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  --,[DayNumberOfMonth]
+  --,[DayNumberOfYear]
+  [WeekNumberOfYear] AS WeekNr, 
+  [EnglishMonthName] AS Month, 
+  LEFT([EnglishMonthName], 3) AS MonthShrt, 
+  --,[SpanishMonthName]
+  --,[FrenchMonthName]
+  [MonthNumberOfYear] AS MonthNr, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year 
+  --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimDate]
+  WHERE CalendarYear >= 2020
